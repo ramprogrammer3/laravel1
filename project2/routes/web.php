6f0ef9a9f91be0e88e18129d6c5f98940512a122
Ram Kumar <rammaniyari@gmail.com>
2024-07-23 11:59:41 +0530

    route with parameter

--- project2/routes/web.php
+++ project2/routes/web.php
@@ -6,16 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post', function () {
-    return view('post');
+Route::get('/post/{id?}/comment/{commentid?}', function (string $id=null,string $commentid = null) {
+    if($id){
+        return "<h1>This is post page id $id </h1> <h2> And comment id is $commentid </h2>";
+    }else{
+        return "<h2>There is no id found in route </h2>";
+    }
 });
 
-Route::get('/post/first', function () {
-    return view('first');
-});
+// Route::get('/post/{id?}', function (string $id=null) {
+//     if($id){
+//         return "<h1>This is post page id $id </h1>";
+//     }else{
+//         return "<h2>There is no id found in route </h2>";
+//     }
+// });
 
-Route::get('/text', function () {
-    return view('about');
-});
+// Route::get('/post/{id}', function (string $id) {
+//     return "<h1>This is post page id $id </h1>";
+// });
 
-// Route::veiw("/text","about");
