65567711c0753ee6dee3e60afc9812142da1186e
Ram Kumar <rammaniyari@gmail.com>
2024-07-23 10:34:48 +0530

    layout

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
