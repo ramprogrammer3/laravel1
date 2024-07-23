6880aa202c265921f3528a861657d43c26e759bd
Ram Kumar <rammaniyari@gmail.com>
2024-07-23 12:38:44 +0530

    ground route

--- project2/routes/web.php
+++ project2/routes/web.php
@@ -6,19 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pages/blog', function () {
-    return view('pages.blog');
-})->name('blog');
+Route::prefix('pages')->group(function () {
 
-Route::get('/pages/post', function () {
-    return view('pages.post');
-})->name('post');
+    Route::get('/blog', function () {
+        return view('pages.blog');
+    })->name('blog');
 
-Route::get('/pages/product', function () {
-    return view('pages.product');
-})->name('product');
+    Route::get('/post', function () {
+        return view('pages.post');
+    })->name('post');
 
+    Route::get('/product', function () {
+        return view('pages.product');
+    })->name('product');
+});
 
-
-
-
+Route::fallback(function () {
+    return '<h1>page not found here </h1>';
+});
