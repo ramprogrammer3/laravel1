9ee8ad763d1e37149c5c553eece68fd0d5585757
ram kumar <rammaniyari@gmail.com>
2024-07-27 15:40:40 +0530

    migration part 3

--- project_db/database/migrations/2024_07_27_092334_create_students_table.php
+++ project_db/database/migrations/2024_07_27_092334_create_students_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->timestamps();
+            $table->string('name',30);
+            $table->string('email',40)->nullable()->unique();
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
