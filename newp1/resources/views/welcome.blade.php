eb76dcf1d751bd9229d4a7867d06b0bfa1ab2b1e
ram kumar <rammaniyari@gmail.com>
2024-08-07 11:07:41 +0530

    route basic part 1

---
+++ newp1/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<h1>Laravel first tutorial here </h1>
+<h1>Laravel first tutorial here </h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi placeat vitae mollitia labore temporibus libero consequuntur. Neque, vero distinctio? Beatae!</p>
+<div>
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem amet quod commodi quos quidem fuga ipsa repudiandae, dignissimos nostrum laborum et vitae consectetur incidunt omnis illum adipisci totam beatae nemo!
+    </p>
+    <p>This  is simple test</p>
+    <h1> This is heading here </h1>
+</div>
+<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt porro eos at iste dolorem distinctio reprehenderit, perspiciatis nemo, voluptate labore magni asperiores neque, aliquid laboriosam aperiam ducimus voluptatum natus fugiat.</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cum rem officia deserunt enim ipsum nostrum dolores esse fugiat. Ex deserunt facere ratione tempora saepe aut nobis assumenda vitae doloribus.</p>
+
+<a href="/post">Post page </a><br>
+<a href="/about">About page </a>
