fea8d3ef40a9d4b8765a8791cb6da28407bc5ec2
ram kumar <rammaniyari@gmail.com>
2024-08-10 13:53:01 +0530

    adding image and javascript in blade template

--- newpp/resources/views/product.blade.php
+++ newpp/resources/views/product.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="col-8 content">
     <h2>Product page here  </h2>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque laboriosam ab unde sequi earum animi impedit quos, dolor aut necessitatibus aperiam eum ducimus veritatis, maxime quod mollitia amet illum iste fugiat officia corporis iure exercitationem accusantium! Asperiores temporibus amet autem in ratione officiis veniam. Fugit delectus atque dignissimos placeat natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ut in neque veritatis ea quisquam maxime. Dolor, illo molestias quam cupiditate hic libero at aliquid, nam quibusdam facilis nesciunt laborum!</p>
+    <p></p>
+    <img src="{{asset('image/19.jpg')}}" alt="" width="120">
 </div>
 @endsection
