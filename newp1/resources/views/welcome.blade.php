39b4a13895b99cf0e3b19e8f29d11c446cf3dc4f
ram kumar <rammaniyari@gmail.com>
2024-08-10 11:30:29 +0530

    named route

--- newp1/resources/views/welcome.blade.php
+++ newp1/resources/views/welcome.blade.php
@@ -11,6 +11,6 @@
 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt porro eos at iste dolorem distinctio reprehenderit, perspiciatis nemo, voluptate labore magni asperiores neque, aliquid laboriosam aperiam ducimus voluptatum natus fugiat.</h3>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cum rem officia deserunt enim ipsum nostrum dolores esse fugiat. Ex deserunt facere ratione tempora saepe aut nobis assumenda vitae doloribus.</p>
 
-<a href="/post">Post page </a><br>
-<a href="/about">About page </a>
+<a href="{{route("mypost")}}">Post page </a><br>
+<a href="{{ route('myabout') }}">About page </a>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem quasi, corrupti facere voluptas sapiente doloremque voluptates modi rerum alias! Dolore, dolorem hic! Quis recusandae repellendus rem odit. Ducimus, alias possimus?</p>
--- newp1/routes/web.php
+++ newp1/routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
 
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name("myabout");
 
 
 Route::get("/post",function(){
     return view('post');
-});
+})->name("mypost");
 
-Route::get("/test/{id?}",function(string $id = null){
-    return "<h1>This is simple test route, Your id is $id </h1>";
-});
+// Route::get("/test/{id?}",function(string $id = null){
+//     return "<h1>This is simple test route, Your id is $id </h1>";
+// })->name("test");
 
