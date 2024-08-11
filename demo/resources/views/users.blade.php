905d1be8001e9eb713f63eaf422f1ffa3076e73b
ram kumar <rammaniyari@gmail.com>
2024-08-11 11:31:11 +0530

    passing data from route to view

---
+++ demo/resources/views/users.blade.php
@@ -0,0 +1,4 @@
+<h1>This is user page here </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero temporibus, non aliquam veniam corrupti repudiandae iste nulla iure modi commodi tempore corporis eos id aspernatur, perferendis natus! Animi, illo voluptate.</p>
+
+<h1>Your name is {{$user}} </h1>
