be428150f3e0fbb026e65348f2b3e21a4b6d422a
ram kumar <rammaniyari@gmail.com>
2024-08-11 12:01:24 +0530

    passing data to view from route

---
+++ demo/resources/views/user.blade.php
@@ -0,0 +1,3 @@
+<h1> Name : {{$id['name']}} </h1>
+<h1> Phone : {{$id['phone']}} </h1>
+<h1> City : {{$id['city']}} </h1>
--- demo/routes/web.php
+++ demo/routes/web.php
@@ -2,11 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
+function getUser()
+{
+    return  [
+        1 => ["name" => "Amitabh", 'phone' => '9725364175', 'city' => 'Goa'],
+        2 => ["name" => "Salman", 'phone' => '9754213698', 'city' => 'Delhi'],
+        3 => ["name" => "Sunny", 'phone' => '9702487563', 'city' => 'Mubmai'],
+        4 => ["name" => "Akshay", 'phone' => '9714752368', 'city' => 'Noida'],
+    ];
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/users', function () {
     $name = "ram kumar maniyari";
-    return view('users',['user' => $name, "city" => "Sitamarhi"]);
+    $names = getUser();
+    return view('users', ['user' => $names, "city" => "Sitamarhi"]);
+
+    // return view('users')->with("user",$name)->with('city', "Noida");
+    // return view('users')
+    //         ->withUser($name)
+    //         ->withCity("Noida");
 });
+
+Route::get('/user/{id}', function ($id) {
+    $users = getUser();
+    abort_if(!isset($users[$id]), 404);
+    $user = $users[$id];
+
+    return view('user', ["id" => $user]);
+})->name('view.user');
