40ee23c4e6dd3ce03a7b0c94e27f1b2bbf961ff9
ram kumar <rammaniyari@gmail.com>
2024-08-24 11:09:28 +0530

    migration part1

---
+++ migration/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel migration tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Laravel migration tutorial</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint reiciendis dolorum fugiat inventore quo esse debitis soluta nisi! Soluta necessitatibus vel voluptatem quo! Obcaecati facilis dolores possimus. Inventore, corrupti? Quaerat?</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
