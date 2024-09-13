2fa5ad9cc6ed8aa62fa019ec5db7245ffca24d4d
ram kumar <rammaniyari@gmail.com>
2024-09-13 17:49:05 +0530

    'blog-app'

---
+++ hasOneOfMany/public/customer.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers(name,email,phone)
+VALUES
+('Ram Kumar','ram@gmail.com',3289432423),
+("Sarita Kumari","sarita@gmail.com",3498234232),
+("Mohit Kumar",'mohit@gmail.com',9083726438),
+("Pihu kumari",'pihu@gmail.com',98324342743)
