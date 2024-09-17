39219c490e256bec7b3464077a28d49076b19795
ram kumar <rammaniyari@gmail.com>
2024-09-17 13:41:57 +0530

    blog app

--- blog_app/routes/web.php
+++ blog_app/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('theme',"layouts.auth");
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
