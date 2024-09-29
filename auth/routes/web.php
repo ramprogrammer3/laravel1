7082f23e9456d9f8e234fb64ae5d54cd0f961789
ram kumar <rammaniyari@gmail.com>
2024-09-29 15:38:05 +0530

    auth in laravel

--- auth/routes/web.php
+++ auth/routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('home',[HomeController::class]);
-
-
